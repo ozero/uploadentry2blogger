40f43bebc27e6f21fd6ed7b0d276dc5b93a4d049
unknown <rgfx@.(none)>
2010-05-04 00:44:13 +0900

    fix

--- entlyuploader.php
+++ entlyuploader.php
@@ -22,14 +22,14 @@ if ($settings['maxuploadperday'] == null) {
 $_obj_sc = new SimpleCRUD($settings['user'], $settings['pass']);
 $_obj_sc->blogID = $settings['blogid'];
 
-
+//upload
 $files = glob($baseset['htmldir'].'/*.html');
 foreach($files as $k0=>$v0){
 	if($k0 > $settings['maxuploadperday']){continue;}
 	print "upload: {$v0}\n";
 	$postID = upload($_obj_sc,$v0);
 	print "done.\n";
-	//rename($v0,$v0.".uploaded_".$postID);
+	rename($v0,$v0.".uploaded_".$postID);
 }
 
 
@@ -40,31 +40,30 @@ function upload(&$blogobj,$path){
 	
 	//extract metadata
 	//<!-- title:my memorial entry -->
-	preg_match("/<!\-\-.title:(.*?).\-\->/","",$htmlcontent,$matches);
+	preg_match("/<!\-\-.title:(.*?).\-\->/",$htmlcontent,$matches);
 	$entrytitle=$matches[1];
 	//<!-- date:2001-01-1T09:00:00+09:00 -->
-	preg_match("/<!\-\-.date:(.*?).\-\->/","",$htmlcontent,$matches);
+	preg_match("/<!\-\-.date:(.*?).\-\->/",$htmlcontent,$matches);
 	$entrydate=$matches[1];
 	
 	//create post
 	$arg=array(
 	    'contenttype'=>'html',
 	    'entrytitle'=>$entrytitle,
-	    'entrycontent'=>join("\n",$htmlcontent),
+	    'entrycontent'=>$htmlcontent,
 	    'is_draft'=>true
 	);
-	print_r($arg);
-	//$postID = $blogobj->createPost($arg);
+	//print_r($arg);
+	$postID = $blogobj->createPost($arg);
 	
 	//add date to the post
-	$entrydate=$matches[1]."T09:0".substr(microtime(),3,1).":00+09:00";
 	$arg=array(
 		'post_id'=>$postID,
 	    'date_published'=>$entrydate,
 	    'date_updated'=>$entrydate,
 	    'is_draft'=>false
 	);
-	print_r($arg);
-	//$updatedPost = $blogobj->updatePost($arg);
+	//print_r($arg);
+	$updatedPost = $blogobj->updatePost($arg);
 	return $postID;
 }
